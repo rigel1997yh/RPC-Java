78ebe7bfa74298eda8440d1818d62d45e5182a51
rigel <710240090@qq.com>
2025-02-17 18:02:41 +0800

    v1-p3 引入zk服务注册，但30行有报错

---
+++ myVersion1/src/main/java/part2/Server/serviceRegister/Impl/ZKServiceRegister.java
@@ -0,0 +1,46 @@
+package part2.Server.serviceRegister.Impl;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import part2.Server.serviceRegister.ServiceRegister;
+
+import java.net.InetSocketAddress;
+
+public class ZKServiceRegister implements ServiceRegister {
+    private CuratorFramework client;
+
+    private static final String ROOT_PATH = "MyRPC";
+
+    public ZKServiceRegister() {
+        RetryPolicy policy = new ExponentialBackoffRetry(1000, 3);
+        this.client = CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(40000).retryPolicy(policy).namespace(ROOT_PATH).build();
+        this.client.start();
+        System.out.println("zookeeper 连接成功");
+
+    }
+
+    @Override
+    public void register(String serviceName, InetSocketAddress serviceAddress) {
+        try {
+            // serviceName创建成永久节点，服务提供者下线时，不删服务名，只删地址
+            if(client.checkExists().forPath("/" + serviceName) == null){
+                client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/" + serviceName);
+            }
+            // 路径地址，一个/代表一个节点
+            String path = "/" + serviceName +"/"+ getServiceAddress(serviceAddress);
+            // 临时节点，服务器下线就删除节点
+            client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path);
+        } catch (Exception e) {
+            System.out.println("此服务已存在");
+        }
+    }
+
+    private String getServiceAddress(InetSocketAddress serviceAddress) {
+        return serviceAddress.getHostName()+":"+serviceAddress.getPort();
+    }
+
+}
